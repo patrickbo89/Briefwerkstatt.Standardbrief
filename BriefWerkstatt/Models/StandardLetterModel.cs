a55fc042bb59622960120aed0d1a1dcd68597783
Pato <Pato@WIN11PC>
2024-04-13 22:59:11 +0200

    Implented basic functionality and UI. PDF Creation missing.

---
+++ BriefWerkstatt/Models/StandardLetterModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BriefWerkstatt.Models
+{
+    public class StandardLetterModel
+    {
+        public SenderModel Sender { get; set; }
+        public RecipientModel Recipient { get; set; }
+        public LetterContentModel LetterContent { get; set; }
+
+        public StandardLetterModel() 
+        {
+            Sender = new SenderModel();
+            Recipient = new RecipientModel();
+            LetterContent = new LetterContentModel() { Intro = "Sehr geehrte Damen und Herren, " };
+        }
+    }
+}
--- BriefWerkstatt/ViewModels/StandardLetterViewModel.cs
+++ BriefWerkstatt/ViewModels/StandardLetterViewModel.cs
@@ -4,38 +4,244 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BriefWerkstatt.ViewModels
 {
     public class StandardLetterViewModel : ViewModelBase
     {
-        private SenderModel? _sender;
-        private RecipientModel? _recipient;
-        private LetterContentModel? _letterContent;
-        private FileInfoModel? _fileInfo;
+        private StandardLetterModel _standardLetter;
+        private FileInfoModel _fileInfo;
+        private Repository.Repository _repository;
 
-        public string SenderName
+        #region Absender-Properties
+        public string? SenderName
         {
-            get { return _sender.Name; }
+            get => _standardLetter.Sender.Name;
             set
             {
-                _sender.Name = value;
+                _standardLetter.Sender.Name = value;
                 OnPropertyChanged(nameof(SenderName));
             }
         }
 
-        public string SenderStreetName
+        public string? SenderStreetName
         {
-            get { return _sender.Name; }
+            get => _standardLetter.Sender.StreetName;
             set
             {
-                _sender.Name = value;
-                OnPropertyChanged(nameof(SenderName));
+                _standardLetter.Sender.StreetName = value;
+                OnPropertyChanged(nameof(SenderStreetName));
             }
         }
 
-        /* TODO: Do not continue like this. Maybe make a ViewModel for each model and combine it into a StandardLetterViewModel
-         *       which will be instantiated in here...
-        */
+        public int SenderStreetNumber
+        {
+            get => _standardLetter.Sender.StreetNumber;
+            set
+            {
+                _standardLetter.Sender.StreetNumber = value;
+                OnPropertyChanged(nameof(SenderStreetNumber));
+            }
+        }
+
+        public int SenderZipCode
+        {
+            get => _standardLetter.Sender.ZipCode;
+            set
+            {
+                _standardLetter.Sender.ZipCode = value;
+                OnPropertyChanged(nameof(SenderZipCode));
+            }
+        }
+
+        public string? SenderCityName
+        {
+            get => _standardLetter.Sender.CityName;
+            set
+            {
+                _standardLetter.Sender.CityName = value;
+                OnPropertyChanged(nameof(SenderCityName));
+            }
+        }
+
+        public string? SenderCareOfInfo
+        {
+            get => _standardLetter.Sender.CareOfInfo;
+            set
+            {
+                _standardLetter.Sender.CareOfInfo = value;
+                OnPropertyChanged(nameof(SenderCareOfInfo));
+            }
+        }
+
+        public string? SenderAdditionalAdressInfo
+        {
+            get => _standardLetter.Sender.AdditionalAdressInfo;
+            set
+            {
+                _standardLetter.Sender.AdditionalAdressInfo = value;
+                OnPropertyChanged(nameof(SenderAdditionalAdressInfo));
+            }
+        }
+        #endregion
+
+        #region Empfänger-Properties
+        public string? RecipientName
+        {
+            get => _standardLetter.Recipient.Name;
+            set
+            {
+                _standardLetter.Recipient.Name = value;
+                OnPropertyChanged(nameof(RecipientName));
+            }
+        }
+
+        public string? RecipientStreetName
+        {
+            get => _standardLetter.Recipient.StreetName;
+            set
+            {
+                _standardLetter.Recipient.StreetName = value;
+                OnPropertyChanged(nameof(RecipientStreetName));
+            }
+        }
+
+        public int RecipientStreetNumber
+        {
+            get => _standardLetter.Recipient.StreetNumber;
+            set
+            {
+                _standardLetter.Recipient.StreetNumber = value;
+                OnPropertyChanged(nameof(RecipientStreetNumber));
+            }
+        }
+
+        public int RecipientZipCode
+        {
+            get => _standardLetter.Recipient.ZipCode;
+            set
+            {
+                _standardLetter.Recipient.ZipCode = value;
+                OnPropertyChanged(nameof(RecipientZipCode));
+            }
+        }
+
+        public string? RecipientCityName
+        {
+            get => _standardLetter.Recipient.CityName;
+            set
+            {
+                _standardLetter.Recipient.CityName = value;
+                OnPropertyChanged(nameof(RecipientCityName));
+            }
+        }
+
+        public string? RecipientCareOfInfo
+        {
+            get => _standardLetter.Recipient.CareOfInfo;
+            set
+            {
+                _standardLetter.Recipient.CareOfInfo = value;
+                OnPropertyChanged(nameof(RecipientCareOfInfo));
+            }
+        }
+
+        public string? RecipientAdditionalAdressInfo
+        {
+            get => _standardLetter.Recipient.AdditionalAdressInfo;
+            set
+            {
+                _standardLetter.Recipient.AdditionalAdressInfo = value;
+                OnPropertyChanged(nameof(RecipientAdditionalAdressInfo));
+            }
+        }
+        #endregion
+
+        #region Briefinhalt-Properties
+        public string? Topic
+        {
+            get => _standardLetter.LetterContent.Topic;
+            set
+            {
+                _standardLetter.LetterContent.Topic = value;
+                OnPropertyChanged(nameof(Topic));
+            }
+        }
+
+        public string? Intro
+        {
+            get => _standardLetter.LetterContent?.Intro;
+            set
+            {
+                _standardLetter.LetterContent.Intro = value;
+                OnPropertyChanged(nameof(Intro));
+            }
+        }
+
+        public string? TextBody
+        {
+            get => _standardLetter.LetterContent.TextBody;
+            set
+            {
+                _standardLetter.LetterContent.TextBody = value;
+                OnPropertyChanged(nameof(TextBody));
+            }
+        }
+
+        public string? Outro
+        {
+            get => _standardLetter.LetterContent.Outro;
+            set
+            {
+                _standardLetter.LetterContent.Outro = value;
+                OnPropertyChanged(nameof(Outro));
+            }
+        }
+        #endregion
+
+        #region Datei-Info-Properties
+        public int CustomerNumber
+        {
+            get => _fileInfo.CustomerNumber;
+            set
+            {
+                _fileInfo.CustomerNumber = value;
+                OnPropertyChanged(nameof(CustomerNumber));
+            }
+        }
+
+        public string? FileName
+        {
+            get => _fileInfo?.FileName;
+            set
+            {
+                _fileInfo.FileName = value;
+                OnPropertyChanged(nameof(FileName));
+            }
+        }
+        #endregion
+
+        #region Commands
+        public void SaveExecute()
+        {
+            SenderName = "Saved!";
+        }
+
+        public ICommand Save
+        {
+            get
+            {
+                return new RelayCommand(SaveExecute);
+            }
+        }
+        #endregion
+
+        public StandardLetterViewModel()
+        {
+            _standardLetter = new StandardLetterModel();
+            _fileInfo = new FileInfoModel();
+            _repository = new Repository.Repository();
+        }
     }
 }
